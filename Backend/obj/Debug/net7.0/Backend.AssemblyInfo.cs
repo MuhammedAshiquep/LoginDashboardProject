add1ccb0dfee0a47b2d170d9fe1a7d70b654c814
Muhammed Ashique <ashique.p@onboard360.com>
2025-07-10 13:54:32 +0530

    error sorted

--- Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508bff8cdbd9d954cdc2072dad4a32322bc7c0b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
