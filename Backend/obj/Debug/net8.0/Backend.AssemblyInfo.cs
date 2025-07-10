fc6cbf87ae97a219781fda76ad7b7916386bec38
Muhammed Ashique <ashique.p@onboard360.com>
2025-07-10 12:56:53 +0530

    Fix .NET SDK version and package compatibility for Railway

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf05d7a4d5052b410ade9b2849585103fc665133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50aeecd81f9f59d899230e27527e4207072783d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
